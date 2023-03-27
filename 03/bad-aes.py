cc393605f853882586f318f5a2abac821ab33eb9
Taka <71265675+takakv@users.noreply.github.com>
2023-03-27 23:17:48 +0300

    Commit hw3 provided code

---
+++ 03/bad-aes.py
@@ -0,0 +1,260 @@
+#note: do not use these methods for any real encryption
+#some methods taken from https://github.com/boppreh/aes/blob/master/aes.py
+
+import  random,os
+
+
+#converts an integer to an byte array
+def int_to_bytes(i,len): # Not optimized
+    res = []
+    for j in range(len):
+        res.append(i%256)
+        i = i>>8
+    return bytes(res)
+
+
+#describes the s_box
+s_box = (
+    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
+)
+
+#describes the inverse s_box
+inv_s_box = (
+    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
+)
+
+
+#performs the sub_bytes operation on an 16-byte array
+def sub_bytes(s):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] = s_box[s[i][j]]
+
+
+#performs the inverse sub_bytes operation on an 16-byte array
+def inv_sub_bytes(s):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] = inv_s_box[s[i][j]]
+
+
+#performs the shiftrows operation  on an 16-byte array
+def shift_rows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
+
+
+#performs the shiftrows operation  on an 16-byte array
+def inv_shift_rows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
+
+
+#performs the addrounds operation  on an 16-byte array
+def add_round_key(s, k):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] ^= k[i][j]
+
+
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    return bytes(sum(matrix, []))
+
+
+#naively generates a random key without keystreaming for
+def keyGen(rounds):
+    keys=[]
+    for i in range(rounds):
+        key=[]
+        for j in range(4):
+            keyRow=[]
+            for k in range(4):
+                bit = random.randint(0,1)
+                keyRow.append(bit)
+            key.append(keyRow)
+        keys.append(key)
+    return keys
+
+
+
+
+
+#this is the bad version of aes, with the mix-columns step omitted.
+def bad_aes(plaintext,rounds,keys):
+    """
+    Encrypts a single block of 16 byte long plaintext.
+    """
+    assert len(plaintext) == 16
+
+    plain_state = bytes2matrix(plaintext)
+
+    add_round_key(plain_state, keys[0])
+
+    for i in range(1, rounds):
+        sub_bytes(plain_state)
+        shift_rows(plain_state)
+        add_round_key(plain_state, keys[i])
+
+    return matrix2bytes(plain_state)
+
+
+
+
+#you won't need this protocol, but included for completeness
+def decrypt_bad_aes(ciphertext,rounds,keys):
+    """
+    Decrypts a single block of 16 byte long ciphertext.
+    """
+    assert len(ciphertext) == 16
+    cipher_state = bytes2matrix(ciphertext)
+    for i in range(rounds - 1, 0, -1):
+        add_round_key(cipher_state, keys[i])
+        inv_shift_rows(cipher_state)
+        inv_sub_bytes(cipher_state)
+
+    add_round_key(cipher_state, keys[0])
+    return matrix2bytes(cipher_state)
+
+
+
+#takes in a vector of messages where each message consists of 16 bytes. samples a random key. encrypts all the messages with bad-aes using this key.
+def encryptVector(queryVector, rounds):
+    keys=keyGen(rounds)
+    resultVec=[]
+    for i in range(len(queryVector)):
+        result=bad_aes(queryVector[i],rounds,keys)
+        resultVec.append(result)
+    return resultVec
+
+#generates k random bytes
+def generate_random_bytes(k):
+    return os.urandom(k)
+
+#this is a lazy evaluation of a random permutation on the message space {0,1}^128, meaning it maps each 128-bitstring to a randomly picked 128-bitstring with no two different inputs mapping to the same output
+#one should submit the input as a vector of 16-byte messages
+def lazyRandomPerm(queryVector):
+    #the vector pairs will contain the input-output pairs
+    pairs=[]
+    #the returnVec will be returned. with returnVec[i]:=pi(queryVector[i])
+    returnVec=[]
+    for i in range(len(queryVector)):
+        #if the queryVector[i] has the wrong length, we give an error
+        assert len(queryVector[i]) == 16
+        answered=False
+        for j in  range(len(pairs)):
+            #here we check whether this value has been queried before and if yes, we return the previous value
+            if (queryVector[i]==pairs[j][0]):
+                returnVec.append(pairs[j][1])
+                answered=True
+        #thus is the case when this element hasn't been queried before
+        if answered==False:
+            outPutIsNew=False
+            #this is a cycle. we sample a new element until we get one that has not been an answer before. it is really unlikely that we stumble upon an element that has been sampled before though.
+            while(not outPutIsNew):
+                outPutCandidate=generate_random_bytes(16)
+                outPutIsNew=True
+                for j in  range(len(pairs)):
+                    if (outPutCandidate==pairs[j][1]):
+                        outPutIsNew=False
+            returnVec.append(outPutCandidate)
+            pairs.append([queryVector[i],outPutCandidate])
+    return returnVec
+
+#this tests whether an element of the input is in the correct format
+def is_valid_input(data):
+    if isinstance(data, bytes) and len(data) == 16:
+        return True
+    else:
+        return False
+
+#this does check the whole array
+def check_inputs(inputs):
+    results = [is_valid_input(data) for data in inputs]
+    return all(results)
+
+
+
+
+
+
+# The game: it gets two parts of an adversary as arguments, it is their task to distinguish between a real (lazily sampled) permutation and the bad AES.
+def guessing_game(adv1,adv2):
+    #adv1 is supposed to produce a vector of messages that will be either encrypted with bad-aes using the same key, or given to a lazily sampled random permutation
+    queryVector=adv1()
+    #if the input is not in the correct format, it is an automatic failure
+    if (not check_inputs(queryVector)):
+        return 0
+    #a random coin is tossed
+    b = random.randint(0,1) # Random bit
+    if (b==0):
+        #if b is 0, we give the query vector to a lazily sampled random permutation
+        resultVector=lazyRandomPerm(queryVector)
+    else:
+        #if b is 1, we encrypt each entry in the query vector with the bad-aes using the same key.
+        resultVector=encryptVector(queryVector, 12)
+    #adv2 gets back the result
+    badv = adv2(resultVector,queryVector)
+    #if adv2 guessed correctly, this returns 1, otherwise it returns 0
+    return b==badv
+
+
+#repeat the guessing_game k times, return the success rate.
+def iteratedGuessingGame(adv1,adv2,k):
+    successCount=0
+    for i in range(k):
+        res=guessing_game(adv1,adv2)
+        successCount+=res
+    rate=1.0*successCount/k
+    print("rate=",rate)
+    return rate
+
+
+#it is your task to fill in this function
+def adv1():
+    ??
+    return queryVector
+
+#it is your task to fill in this function
+def adv2(resultVector,queryVector):
+    ??
+    return badv
+
+
+iteratedGuessingGame(adv1,adv2,3000)
